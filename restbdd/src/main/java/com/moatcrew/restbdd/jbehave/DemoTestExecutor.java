2037064ac75ef822cf5554584cc3bb45f6bfa3e3
Tenkensui <tenshikhimari@gmail.com>
2016-08-01 15:58:19 +0200

    Jbehave story stuff

--- restbdd/src/main/java/com/moatcrew/restbdd/jbehave/DemoTestExecutor.java
+++ restbdd/src/main/java/com/moatcrew/restbdd/jbehave/DemoTestExecutor.java
@@ -1,7 +1,11 @@
 package com.moatcrew.restbdd.jbehave;
 
+import org.springframework.test.context.ContextConfiguration;
+
 /**
  * Created by tensh on 31/07/2016.
  */
-public class DemoTestExecutor {
+
+@ContextConfiguration(locations = "classpath:spring/application-context.xml")
+public class DemoTestExecutor extends SpringJbehaveStory {
 }
